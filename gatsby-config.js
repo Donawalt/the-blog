63caaa3e63d49f82f927c0b38b8af7102aecdd0c
Donaël Walt <w.dodo.a@hotmail.fr>
2019-06-08 22:34:15 +0200

    add fullscreen support PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,7 +129,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `fullscreen`,
         icon: `content/assets/dona-icon.png`,
       },
     },
